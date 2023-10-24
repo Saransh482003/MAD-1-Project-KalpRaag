02850a95926636a79f6d905f1d0e68c0b2c29f95
Saransh <saini.saransh03@gmail.com>
2023-10-24 22:25:08 +0530

    Search bar fixed

--- app/models.py
+++ app/models.py
@@ -8,3 +8,7 @@ class Users(db.Model):
     user_password = db.Column(db.String, nullable=False)
     creator_id = db.Column(db.Integer, nullable=True, unique=True)
     playlists = db.Column(db.String,nullable=True)
+
+class Songs(db.Model):
+    song_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    song_name = db.Column(db.String, nullable=False)
