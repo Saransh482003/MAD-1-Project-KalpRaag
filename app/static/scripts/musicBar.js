c2903dcc34c4f7fce2cde559dd4b632e479336d3
Saransh <saini.saransh03@gmail.com>
2023-10-31 19:21:27 +0530

    music bar

--- app/static/scripts/musicBar.js
+++ app/static/scripts/musicBar.js
@@ -1,8 +1,22 @@
+var audio = new Audio('../static/songs/Badshaho.mp3');
+var controlClicked = false
 $(document).ready(function () {
     $('.update-content-link').click(function (event) {
         event.preventDefault();
+        controlClicked = true
+        controler.src = "../static/images/pause.png"
+        controler.alt = "Pause"
 
         var songId = $(this).data('song-id');
+        if (!audio.paused){
+            audio.pause()
+            audio.currentTime = 0
+            audio.play()
+        }
+        else {
+            audio.currentTime = 0
+            audio.play()
+        }
 
         $.get('/update-section?song_id=' + songId, function (data) {
             $('#musicImgBottom').html(data["song_name"][0].toUpperCase());
@@ -14,17 +28,31 @@ $(document).ready(function () {
     });
 });
 
-var controlClicked = false
+
+
+
 var controler = document.getElementById("musicBarControl")
 var controlerCont = document.getElementById("controler")
 controlerCont.addEventListener("click", ()=>{
     controlClicked = !controlClicked
     if (controlClicked){
-        controler.src = "pause.png"
+        controler.src = "../static/images/pause.png"
         controler.alt = "Pause"
+        if (audio.paused){
+            audio.play()
+        }
     }
     else{
-        controler.src = "play.png"
+        controler.src = "../static/images/play.png"
         controler.alt = "Play"
+        if (!audio.paused){
+            audio.pause()
+        }
     }
+})
+
+var controlerBack = document.getElementById("musicBarControlBack")
+var controlerContBack = document.getElementById("controlBack")
+controlerContBack.addEventListener("click", ()=>{
+    audio.currentTime = 0
 })
