605ed195514bb82ceca1847c9c7ec15c0db0e80b
Saransh <saini.saransh03@gmail.com>
2023-11-02 12:57:05 +0530

    Raed Lyrics started

--- app/static/scripts/lyrics.js
+++ app/static/scripts/lyrics.js
@@ -1,7 +1,5 @@
 let press = document.getElementById("press")
-var audio = new Audio('../static/songs/Badshaho.mp3');
-press.addEventListener("click",()=>{
-    let time = press.getAttribute("data-time")
-    audio.currentTime = time
-    audio.play()
-})
+var audio = new Audio('../../static/songs/Badshaho.mp3');
+let time = press.getAttribute("data-time")
+audio.currentTime = time
+audio.play()
--- app/static/scripts/musicBar.js
+++ app/static/scripts/musicBar.js
@@ -1,18 +1,21 @@
 var audio = new Audio('../static/songs/Badshaho.mp3');
 var controlClicked = false
-// var controler = document
+var playClicked = false
+var songId = 0
+var userName = ""
 
 $(document).ready(function () {
     $('.update-content-link').click(function (event) {
         event.preventDefault();
         controlClicked = true
+        playClicked = true
         musicBarPlayPause.src = "../static/images/pause.png"
         musicBarPlayPause.alt = "Pause"
 
-        var songId = $(this).data('song-id');
-        var userName = $(this).data('user-name');
+        songId = $(this).data('song-id');
+        userName = $(this).data('user-name');
         console.log(userName)
-        if (!audio.paused){
+        if (!audio.paused) {
             audio.pause()
             audio.currentTime = 0
             audio.play()
@@ -21,64 +24,76 @@ $(document).ready(function () {
             audio.currentTime = 0
             audio.play()
         }
+
+        // alert(current_time)
         $.get('/update-section?song_id=' + songId, function (data) {
             $('#musicImgBottom').html(data["song_name"][0].toUpperCase());
-            $('#musicNameBottom').html(data["song_name"][0].toUpperCase()+data["song_name"].slice(1,));
+            $('#musicNameBottom').html(data["song_name"][0].toUpperCase() + data["song_name"].slice(1,));
             $('#musicArtistBottom').html(data["artist_id"]);
             $('#musicDurationBottom').html(data["duration"]);
-            $('#lyrics').attr("href",`./${userName}/lyrics?song_id=${songId}&current_time=${audio.currentTime}`)
+            // $('#lyrics').attr("href", `./${userName}/lyrics?song_id=${songId}`)
         });
     });
 });
 
+let lyrics = document.getElementById("lyrics")
+lyrics.addEventListener("click", ()=> {
+    if (playClicked){
+        window.location.href =  `./${userName}/lyrics?song_id=${songId}&current_time=${audio.currentTime}`;
+    }
+
+    // console.log(`./${userName}/lyrics?song_id=${songId}&current_time=${audio.currentTime}`);
+
+    // alert(lyrics.href)
+})
 
 let musicBarPlayPause = document.getElementById("musicBarPlayPause")
 let playPause = document.getElementById("playPause")
-playPause.addEventListener("click", ()=>{
+playPause.addEventListener("click", () => {
     controlClicked = !controlClicked
-    if (controlClicked){
+    if (controlClicked) {
         musicBarPlayPause.src = "../static/images/pause.png"
         musicBarPlayPause.alt = "Pause"
-        if (audio.paused){
+        if (audio.paused) {
             audio.play()
-            
+
             console.log(audio.currentTime)
         }
     }
-    else{
+    else {
         musicBarPlayPause.src = "../static/images/play.png"
         musicBarPlayPause.alt = "Play"
-        if (!audio.paused){
+        if (!audio.paused) {
             audio.pause()
         }
     }
 })
 
 let previous = document.getElementById("previous")
-previous.addEventListener("click", ()=>{
+previous.addEventListener("click", () => {
     audio.currentTime = 0
-    if (audio.paused){
+    if (audio.paused) {
         audio.play()
     }
 })
 
 let forward = document.getElementById("forward")
-forward.addEventListener("click", ()=>{
-    audio.currentTime = audio.duration-1
+forward.addEventListener("click", () => {
+    audio.currentTime = audio.duration - 1
 })
 
 let previous10Sec = document.getElementById("previous10Sec")
-previous10Sec.addEventListener("click", ()=>{
-    audio.currentTime = (audio.currentTime-10>=0) ? audio.currentTime-10 : 0;
-    if (audio.paused){
+previous10Sec.addEventListener("click", () => {
+    audio.currentTime = (audio.currentTime - 10 >= 0) ? audio.currentTime - 10 : 0;
+    if (audio.paused) {
         audio.play()
     }
 })
 
 let forward10Sec = document.getElementById("forward10Sec")
-forward10Sec.addEventListener("click", ()=>{
-    audio.currentTime = (audio.currentTime+10<=audio.duration) ? audio.currentTime+10 : audio.duration-1;
-    if (audio.paused){
+forward10Sec.addEventListener("click", () => {
+    audio.currentTime = (audio.currentTime + 10 <= audio.duration) ? audio.currentTime + 10 : audio.duration - 1;
+    if (audio.paused) {
         audio.play()
     }
 })
